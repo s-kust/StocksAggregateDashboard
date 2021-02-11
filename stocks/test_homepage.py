de3fdbfcd4d9e38b4786bd8b81c390fc4400d1fd
Serhii Kushchenko <send2kust@gmail.com>
2021-02-11 11:08:40 +0100

    split one big homepage test into two for title and body separately

--- stocks/test_homepage.py
+++ stocks/test_homepage.py
@@ -2,20 +2,23 @@ from selenium import webdriver
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
-    def setUp(self):
+    
+    @classmethod
+    def setUpClass(self):
         self.browser = webdriver.Firefox()
+        self.browser.get('http://stocks.pp.ua/')
 
-    def tearDown(self):
+    @classmethod
+    def tearDownClass(self):
         self.browser.quit()
   
-    def test_homepage_contains_sectors_data_table(self):
-        self.browser.get('http://stocks.pp.ua/')
-# print(browser.page_source)
-# print('Now cookies')
-# print(browser.get_cookies())
-
+    def test_homepage_title(self):   
+        print('External title')
         self.assertIn ('Stocks and Sectors Data', self.browser.title)
         self.assertIn ('Home', self.browser.title)
+    
+    def test_homepage_body(self):
+        print('External body')
         self.assertIn ('Here you can choose stocks for long and short positions', self.browser.page_source)
         self.assertIn ('Utilities', self.browser.page_source)
         self.assertIn ('Avg. Dividend Yield', self.browser.page_source)
@@ -23,3 +26,6 @@ class NewVisitorTest(unittest.TestCase):
 if __name__ == '__main__':
     # unittest.main(warnings='ignore')
     unittest.main()
+    
+# print(browser.page_source)
+# print(browser.get_cookies())    
