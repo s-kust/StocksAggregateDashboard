0847936bd049b7a2cc05bdb033bb50f545bd7625
Serhii Kushchenko <send2kust@gmail.com>
2021-02-11 13:29:12 +0100

    more tests

--- stocks/test_homepage.py
+++ stocks/test_homepage.py
@@ -2,18 +2,23 @@ from selenium import webdriver
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
-    def setUp(self):
+    
+    @classmethod
+    def setUpClass(self):
         self.browser = webdriver.Firefox()
         self.browser.get('http://stocks.pp.ua/')
 
-    def tearDown(self):
+    @classmethod
+    def tearDownClass(self):
         self.browser.quit()
   
-    def test_homepage_title(self):       
+    def test_homepage_title(self):   
+        print('External title')
         self.assertIn ('Stocks and Sectors Data', self.browser.title)
         self.assertIn ('Home', self.browser.title)
     
     def test_homepage_body(self):
+        print('External body')
         self.assertIn ('Here you can choose stocks for long and short positions', self.browser.page_source)
         self.assertIn ('Utilities', self.browser.page_source)
         self.assertIn ('Avg. Dividend Yield', self.browser.page_source)
