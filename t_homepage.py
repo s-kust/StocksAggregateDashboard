465ea619cc2f4f52cba8714283c33a7fa9f89064
Serhii Kushchenko <send2kust@gmail.com>
2021-02-11 11:05:56 +0100

    start using unittest

--- t_homepage.py
+++ t_homepage.py
@@ -1,13 +1,25 @@
 from selenium import webdriver
+import unittest
+
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
   
-browser = webdriver.Firefox()
-browser.get('http://stocks.pp.ua/')
+    def test_homepage_contains_sectors_data_table(self):
+        self.browser.get('http://stocks.pp.ua/')
 # print(browser.page_source)
 # print('Now cookies')
 # print(browser.get_cookies())
 
-assert 'Stocks and Sectors Data' in browser.title
-assert 'Home' in browser.title
-assert 'Here you can choose stocks for long and short positions' in browser.page_source
-assert 'Avg. Dividend Yield' in browser.page_source
-assert 'Utilities' in browser.page_source
+        self.assertIn ('Stocks and Sectors Data', self.browser.title)
+        self.assertIn ('Home', self.browser.title)
+        self.assertIn ('Here you can choose stocks for long and short positions', self.browser.page_source)
+        self.assertIn ('Utilities', self.browser.page_source)
+        self.assertIn ('Avg. Dividend Yield', self.browser.page_source)
+
+if __name__ == '__main__':
+    # unittest.main(warnings='ignore')
+    unittest.main()
