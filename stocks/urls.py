f03aef601e407fe21da7394ae2a693b5f2a579c3
Serhii Kushchenko <send2kust@gmail.com>
2021-02-16 16:00:21 +0100

    In URLs changed from IDs to slugs

--- stocks/urls.py
+++ stocks/urls.py
@@ -7,10 +7,15 @@ from django.views.decorators.cache import cache_page
 
 urlpatterns = [
 path('', HomePageView.as_view(), name='home'),
-path('sector/<int:sector_id>/', cache_page(60 * 15)(views.sectordetail), name='sectordetail'),
-path('industry/<int:industry_id>/leverage/', views.industrydetailleverage, name='industrydetailleverage'), # leverage
-path('industry/<int:industry_id>/forward-pe/', views.industrydetailpe, name='industrydetailpe'),
-path('industry/<int:industry_id>/margin/', views.industrydetailmargin, name='industrydetailmargin'),
+# path('sector/<int:sector_id>/', cache_page(60 * 15)(views.sectordetail), name='sectordetail'),
+# path('sector/<int:sector_id>/', views.sectordetail, name='sectordetail'),
+path('sector/<slug:slug>/', views.sectordetail, name='sectordetail'),
+# path('industry/<int:industry_id>/leverage/', views.industrydetailleverage, name='industrydetailleverage'), # leverage
+# path('industry/<int:industry_id>/forward-pe/', views.industrydetailpe, name='industrydetailpe'),
+# path('industry/<int:industry_id>/margin/', views.industrydetailmargin, name='industrydetailmargin'),
+path('industry/<slug:slug>/leverage/', views.industrydetailleverage, name='industrydetailleverage'), # leverage
+path('industry/<slug:slug>/forward-pe/', views.industrydetailpe, name='industrydetailpe'),
+path('industry/<slug:slug>/margin/', views.industrydetailmargin, name='industrydetailmargin'),
 path('company/<slug:ticker>/', views.companydetail, name='companydetail'),
 path('search/', SearchResultsListView.as_view(), name='search_results'),
 path('about/', TemplateView.as_view(template_name="about.html"), name='about'),
